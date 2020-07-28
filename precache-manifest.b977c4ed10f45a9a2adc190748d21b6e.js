eae5d9a0a3d4b243606056e3effe1bc1abf63c8a
unknown <yaroslavdutka@gmail.com>
2020-07-28 16:33:02 +0300

    Updates

---
+++ precache-manifest.b977c4ed10f45a9a2adc190748d21b6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7056aae31936643efded6d4f557b054a",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "17e421a0f021c8d71a43",
+    "url": "/goit-react-hw-01-components/static/css/main.47956d97.chunk.css"
+  },
+  {
+    "revision": "bf90318edf4aaa639ebd",
+    "url": "/goit-react-hw-01-components/static/js/2.cebad5db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.cebad5db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17e421a0f021c8d71a43",
+    "url": "/goit-react-hw-01-components/static/js/main.33c4f0c2.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
